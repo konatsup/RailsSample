5b2704c3dfa34aa1f869ef7831b578c8b080258d
Konatsup <konatsukomatsu@gmail.com>
2017-12-12 20:39:41 +0900

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
-   	  log_in user
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない    	
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
   	end
   end
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,7 +10,21 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
-  include ApplicationHelper
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
 
+  include ApplicationHelper
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
